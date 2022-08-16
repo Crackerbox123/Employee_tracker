6f0c9ad008b801d1a1ca5e80f54ede7ee502b16e
Kal O <koestreicher4@gmail.com>
2022-08-16 00:22:17 -0500

    npm start fix

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
+
+
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const db = require('./db/connection');
+
+
+
+// start server after db connection
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+    app.listen(PORT, () => {
+      console.log(`server running on port ${PORT}`);
+    });
+  });
